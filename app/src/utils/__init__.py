bdedde3c41f3996a02897b2817da4b07111d13a7
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-19 06:40:53 +0100

    Add module docstrings to utils and crypt modules for improved clarity

--- app/src/utils/__init__.py
+++ app/src/utils/__init__.py
@@ -1,1 +1,8 @@
+"""
+This module initializes the utils package and imports all functions and classes from the crypt module.
+
+Modules:
+    crypt: Contains cryptographic utility functions and classes.
+"""
+
 from .crypt import *
