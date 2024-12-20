c7ff5b2643459e42ff9c28d5d544f1e74b0521b4
Victory Chibuike <victorychibuike121@gmail.com>
2024-12-20 16:10:43 +0100

    Bad code: Bug exists!

--- app/tests/test_user/test_user_data.py
+++ app/tests/test_user/test_user_data.py
@@ -20,6 +20,14 @@ def user_data():
         password=fake.password(),
     )
 
+@fixture
+async def created_user(user_data: UserCreate):
+    """
+    Fixture to create a user in the database
+    """
+    user_repo = UserRepository()
+    return await user_repo.create_user(user_data)
+
 
 @pytest.mark.asyncio
 async def test_create_user(user_data: UserCreate):
@@ -38,3 +46,84 @@ async def test_create_user(user_data: UserCreate):
     assert user["hashed_password"]
     with raises(Exception):
         assert user["password"] == user_data.password
+
+
+@pytest.mark.asyncio
+async def test_get_all_users(user_data):
+    user_repo = UserRepository()
+
+    # Clean the database and add users
+    await async_db.users.delete_many({})
+    for _ in range(3):
+        await user_repo.create_user(user_data)
+
+    # Get all users
+    users = await user_repo.get_all_users()
+
+    # Assertions
+    assert len(users) == 3
+    for user in users:
+        assert user["_id"]
+        assert user["email"] == user_data.email
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_id(created_user):
+    user_repo = UserRepository()
+
+    # Fetch user by ID
+    user = await user_repo.get_user_by_id(created_user["_id"])
+
+    # Assertions
+    assert user
+    assert user["_id"] == created_user["_id"]
+    assert user["email"] == created_user["email"]
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_phone_number(created_user):
+    user_repo = UserRepository()
+
+    # Fetch user by phone number
+    user = await user_repo.get_user_by_phone_number(created_user["phone_number"])
+
+    # Assertions
+    assert user
+    assert user["_id"] == created_user["_id"]
+    assert user["phone_number"] == created_user["phone_number"]
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_email(created_user):
+    user_repo = UserRepository()
+
+    # Fetch user by email
+    user = await user_repo.get_user_by_email(created_user["email"])
+
+    # Assertions
+    assert user
+    assert user["_id"] == created_user["_id"]
+    assert user["email"] == created_user["email"]
+
+
+@pytest.mark.asyncio
+async def test_update_user(created_user):
+    user_repo = UserRepository()
+
+    # Modify the user and update
+    created_user["first_name"] = "UpdatedName"
+    updated_user = await user_repo.update_user(created_user)
+
+    # Assertions
+    assert updated_user["first_name"] == "UpdatedName"
+    assert updated_user["date_updated"] > updated_user["date_created"]
+
+
+@pytest.mark.asyncio
+async def test_delete_user(created_user):
+    user_repo = UserRepository()
+
+    await user_repo.delete_user(created_user["_id"])
+    user = await user_repo.get_user_by_id(created_user["_id"])
+    assert user is None
+
