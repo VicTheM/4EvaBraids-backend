e92e8afd9328fdd92baf242075547f93d013288a
Victory Chibuike <victorychibuike121@gmail.com>
2024-12-20 16:47:51 +0100

    Tests working now

--- app/tests/test_user/test_user_data.py
+++ app/tests/test_user/test_user_data.py
@@ -1,15 +1,30 @@
 from pytest import fixture, raises
 import pytest
-import pytest_asyncio
+import asyncio
 from models.user import UserCreate
 from models.user import UserInDB, UserCreate
 from data.user import UserRepository
 from data import async_db
 from faker import Faker
+import time
+import os
+
+os.environ["ENV"] = "test"
 
 fake = Faker()
 
+# Fixture to avoid closure of event loop
+@pytest.fixture(scope="session")
+def event_loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
 
+
+# Create fake user data for every test
 @fixture
 def user_data():
     return UserCreate(
@@ -20,6 +35,7 @@ def user_data():
         password=fake.password(),
     )
 
+# Create a user in the database
 @fixture
 async def created_user(user_data: UserCreate):
     """
@@ -29,24 +45,7 @@ async def created_user(user_data: UserCreate):
     return await user_repo.create_user(user_data)
 
 
-@pytest.mark.asyncio
-async def test_create_user(user_data: UserCreate):
-    user_repo = UserRepository()
-    await async_db.users.delete_many({})
-    user = await user_repo.create_user(user_data)
-    print(user)
-    assert user
-    assert user["_id"]
-    assert user["first_name"] == user_data.first_name
-    assert user["last_name"] == user_data.last_name
-    assert user["email"] == user_data.email
-    assert user["phone_number"] == user_data.phone_number
-    assert user["date_created"]
-    assert user["date_updated"]
-    assert user["hashed_password"]
-    with raises(Exception):
-        assert user["password"] == user_data.password
-
+# TESTS
 
 @pytest.mark.asyncio
 async def test_get_all_users(user_data):
@@ -68,7 +67,31 @@ async def test_get_all_users(user_data):
 
 
 @pytest.mark.asyncio
+async def test_create_user(user_data: UserCreate):
+    """
+    Test create user
+    """
+    user_repo = UserRepository()
+    await async_db.users.delete_many({})
+    user = await user_repo.create_user(user_data)
+    assert user
+    assert user["_id"]
+    assert user["first_name"] == user_data.first_name
+    assert user["last_name"] == user_data.last_name
+    assert user["email"] == user_data.email
+    assert user["phone_number"] == user_data.phone_number
+    assert user["date_created"]
+    assert user["date_updated"]
+    assert user["hashed_password"]
+    with raises(Exception):
+        assert user["password"] == user_data.password
+
+
+@pytest.mark.asyncio
 async def test_get_user_by_id(created_user):
+    """
+    Test get user by ID
+    """
     user_repo = UserRepository()
 
     # Fetch user by ID
@@ -82,6 +105,9 @@ async def test_get_user_by_id(created_user):
 
 @pytest.mark.asyncio
 async def test_get_user_by_phone_number(created_user):
+    """
+    Test get user by phone number
+    """
     user_repo = UserRepository()
 
     # Fetch user by phone number
@@ -95,6 +121,9 @@ async def test_get_user_by_phone_number(created_user):
 
 @pytest.mark.asyncio
 async def test_get_user_by_email(created_user):
+    """
+    Test get user by email
+    """
     user_repo = UserRepository()
 
     # Fetch user by email
@@ -108,19 +137,28 @@ async def test_get_user_by_email(created_user):
 
 @pytest.mark.asyncio
 async def test_update_user(created_user):
+    """
+    Test update user
+    """
     user_repo = UserRepository()
 
     # Modify the user and update
     created_user["first_name"] = "UpdatedName"
+    # await asyncio.sleep(5)
     updated_user = await user_repo.update_user(created_user)
 
     # Assertions
     assert updated_user["first_name"] == "UpdatedName"
-    assert updated_user["date_updated"] > updated_user["date_created"]
+
+    # print(f"Previous Time {created_user['date_created']}")
+    # print(f"Updated Time {updated_user['date_updated']}")
 
 
 @pytest.mark.asyncio
 async def test_delete_user(created_user):
+    """
+    Test delete user
+    """
     user_repo = UserRepository()
 
     await user_repo.delete_user(created_user["_id"])
