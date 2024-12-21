d4118598d91916ffb68cbdf15c1fca1ab2a4039d
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-21 07:04:15 +0100

    Refactor user data fixtures and enhance test documentation

--- app/tests/test_user/test_user_data.py
+++ app/tests/test_user/test_user_data.py
@@ -8,20 +8,29 @@ os.environ["ENV"] = "test"
 from data import async_db
 from data.user import UserRepository
 from faker import Faker
+import datetime as dt
 
 fake = Faker()
 
 
 # Create fake user data for every test
 @fixture
-def user_data():
-    return UserCreate(
+async def user_data():
+    """
+    Fixture to create fake user data
+
+    Returns:
+        UserCreate: UserCreate model
+    """
+    await async_db.users.delete_many({})
+    yield UserCreate(
         first_name=fake.first_name(),
         last_name=fake.last_name(),
         email=fake.email(),
         phone_number=fake.phone_number()[:15],
         password=fake.password(),
     )
+    await async_db.users.delete_many({})
 
 
 # Create a user in the database
@@ -29,40 +38,28 @@ def user_data():
 async def created_user(user_data: UserCreate):
     """
     Fixture to create a user in the database
+
+    Args:
+        user_data (UserCreate): UserCreate model
     """
+    await async_db.users.delete_many({})
     user_repo = UserRepository()
-    return await user_repo.create_user(user_data)
+    yield await user_repo.create_user(user_data)
+    await async_db.users.delete_many({})
 
 
 # TESTS
 
 
 @pytest.mark.anyio
-async def test_get_all_users(user_data):
-    user_repo = UserRepository()
-
-    # Clean the database and add users
-    await async_db.users.delete_many({})
-    for _ in range(3):
-        await user_repo.create_user(user_data)
-
-    # Get all users
-    users = await user_repo.get_all_users()
-
-    # Assertions
-    assert len(users) == 3
-    for user in users:
-        assert user["_id"]
-        assert user["email"] == user_data.email
-
-
-@pytest.mark.anyio
 async def test_create_user(user_data: UserCreate):
     """
     Test create user
+
+    Args:
+        user_data (UserCreate): UserCreate model
     """
     user_repo = UserRepository()
-    await async_db.users.delete_many({})
     user = await user_repo.create_user(user_data)
     assert user
     assert user["_id"]
@@ -78,9 +75,34 @@ async def test_create_user(user_data: UserCreate):
 
 
 @pytest.mark.anyio
+async def test_get_all_users(user_data):
+    """
+    Test get all users
+
+    Args:
+        user_data (UserCreate): UserCreate model
+    """
+    user_repo = UserRepository()
+    for _ in range(3):
+        await user_repo.create_user(user_data)
+
+    # Get all users
+    users = await user_repo.get_all_users()
+
+    # Assertions
+    assert len(users) == 3
+    for user in users:
+        assert user["_id"]
+        assert user["email"] == user_data.email
+
+
+@pytest.mark.anyio
 async def test_get_user_by_id(created_user):
     """
     Test get user by ID
+
+    Args:
+        created_user (UserInDB): UserInDB model
     """
     user_repo = UserRepository()
 
@@ -97,6 +119,9 @@ async def test_get_user_by_id(created_user):
 async def test_get_user_by_phone_number(created_user):
     """
     Test get user by phone number
+
+    Args:
+        created_user (UserInDB): UserInDB model
     """
     user_repo = UserRepository()
 
@@ -115,6 +140,9 @@ async def test_get_user_by_phone_number(created_user):
 async def test_get_user_by_email(created_user):
     """
     Test get user by email
+
+    Args:
+        created_user (UserInDB): UserInDB model
     """
     user_repo = UserRepository()
 
@@ -131,25 +159,31 @@ async def test_get_user_by_email(created_user):
 async def test_update_user(created_user):
     """
     Test update user
+
+    Args:
+        created_user (UserInDB): UserInDB model
     """
     user_repo = UserRepository()
 
     # Modify the user and update
     created_user["first_name"] = "UpdatedName"
-    # await asyncio.sleep(5)
+    initial_date_updated = created_user["date_updated"].replace(
+        tzinfo=dt.timezone.utc
+    )
     updated_user = await user_repo.update_user(created_user)
 
     # Assertions
     assert updated_user["first_name"] == "UpdatedName"
-
-    # print(f"Previous Time {created_user['date_created']}")
-    # print(f"Updated Time {updated_user['date_updated']}")
+    assert updated_user["date_updated"] > initial_date_updated
 
 
 @pytest.mark.anyio
 async def test_delete_user(created_user):
     """
     Test delete user
+
+    Args:
+        created_user (UserInDB): UserInDB model
     """
     user_repo = UserRepository()
 
