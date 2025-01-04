15cdc2cf2d00689db6c9875ff4fa823995c64eb7
Victory Chibuike <victorychibuike121@gmail.com>
2025-01-04 05:29:17 +0100

    Added model for configuring email message

---
+++ app/src/models/email.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+from config import settings
+from typing import List
+
+class EmailemailSettings(BaseModel):
+    """
+    This class Holds configuration for sending emails
+    """
+    MAIL_USERNAME: str = settings.EMAIL
+    MAIL_PASSWORD: str = settings.PASSWORD
+    MAIL_FROM: EmailStr = settings.EMAIL
+    MAIL_FROM_NAME: str = "4evabraids"
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False      # This might have to chnage to True when we deploy
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = False
+    RECEIVERS_EMAIL: List[EmailStr] = ["victorychibuike111@gmail.com", "virginiangozi08@gmail.com"]
