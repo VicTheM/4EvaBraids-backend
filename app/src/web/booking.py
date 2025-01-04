07f50a333cdc243dbbf5405bdf812cbe54aa43fa
Victory Chibuike <victorychibuike121@gmail.com>
2025-01-04 06:42:40 +0100

    Improved API that receives booking request

--- app/src/web/booking.py
+++ app/src/web/booking.py
@@ -1,39 +1,61 @@
+"""
+This file defines the route used to book an appointment
+"""
 from fastapi import APIRouter
 from fastapi import BackgroundTasks
-from fastapi_mail import FastMail, MessageSchema
-from pydantic import EmailStr
 from utils.send_mail import send_email
+from web.auth import oauth2_scheme
+from fastapi import HTTPException, Depends, status
+from controllers import user as user_controller
+import jwt
+from config import settings
 
 router = APIRouter(prefix="/bookings", tags=["bookings"])
 
 
 @router.post("/")
-async def create_appointment(background_tasks: BackgroundTasks, email: EmailStr):
-    name = "John Doe"
-    phone = "08012345678"
-    date = "2021-12-01"
-    time = "10:00 AM"
-    location = "Lekki Phase 1"
-    style = "Braids"
+async def create_appointment(background_tasks: BackgroundTasks, location: str = "unknown",
+                             style: str = "Unknown", date: str = "01-01-2025", time: str = "00.00am",
+                             token: str = Depends(oauth2_scheme)):
+    """
+    Create an appointment. Get redirected for authentication if not loggedin
+    """
 
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        user_id = payload.get("user_id")
+        if user_id is None:
+            raise credentials_exception
+    except jwt.PyJWTError:
+        raise credentials_exception
+    
+    user = await user_controller.get_user_by_id(user_id)
+    if user is None:
+        raise credentials_exception
+
+    # Build up the message to be sent
     subject = "You have a new appointment"
     content = f'''
     Hello, you have a booking with the following details:
     
-    Name:   {name}
-    Phone:  {phone}
-    Email:  {email}
-    Date:   {date}
-    Time:   {time}
+    Name:   {user.first_name} {user.last_name}
+    Phone:  {user.phone_number}
+    Email:  {user.email}
+    Desired Date:   {date}
+    Desired timeTime:   {time}
     Location:   {location}
     Style:  {style}
-
     Do well to contact her as soon as possible!
     '''
 
     try:
         await send_email(background_tasks, subject, content)
-        # Save order to database in the future
         return {"message": "Email sent successfully"}
     except Exception as e:
-        return {"message": "An error occurred while sending the email."}
+        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
